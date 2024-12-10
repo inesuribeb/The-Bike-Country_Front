8dfff72efe2c1353fe26a7857236bee7161883f3
Yerbrown <yerbrown98@gmail.com>
2024-12-10 15:25:50 +0100

    Change page by state

--- mi-aplicacion/src/main.jsx
+++ mi-aplicacion/src/main.jsx
@@ -1,16 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import NavBar from "./components/navbar/NavBar";
-import Sections from "./components/sections/sections";
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <header>
-            <NavBar></NavBar>
-        </header>
-        <main>
-            <Sections></Sections>
-        </main>
+        <App />
     </StrictMode>
 );
