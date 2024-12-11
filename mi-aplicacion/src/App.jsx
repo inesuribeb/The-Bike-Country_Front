442bba7ff61c9a9fbe69c7f9694d13903e7e2168
Yerbrown <yerbrown98@gmail.com>
2024-12-11 12:13:23 +0100

    Api integration and context for loading multiple pages

--- mi-aplicacion/src/App.jsx
+++ mi-aplicacion/src/App.jsx
@@ -1,20 +1,45 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+import { PageContext } from "./utils/js/context/PageContext.js";
 import "./App.css";
 import Home from "./pages/home/Home";
 import Experiences from "./pages/experiences/Experiences";
+// import Contact from "./pages/contact/Contact";
+import { obtenerPacks, workerLogin } from "./utils/js/apiCallController.js";
 
 function App() {
     const [count, setCount] = useState(0);
-    const [page, setPage] = useState("experiences");
+    const [page, setPage] = useState("home");
+    const [packs, setPacks] = useState(null);
+    useEffect(() => {
+        async function getPacks() {
+            const data = await obtenerPacks();
+            console.log("Packs obtenidos:", data);
+            setPacks(data);
+        }
+        async function loginByWorker() {
+            const data = await workerLogin();
+            console.log("Token obtenido:", data);
+        }
+        loginByWorker();
+    }, []);
+    useEffect(() => {
+        console.log("Page cambiada a:", page);
+        window.scrollTo({
+            top: 0,
+        });
+    }, [page]);
     const components = {
-        home: Home, 
-        experiences: Experiences
+        home: Home,
+        experiences: Experiences,
+        // contact: Contact,
     };
 
     const Component = components[page];
-    return <Component />;
+    return (
+        <PageContext.Provider value={{ page, setPage }}>
+            <Component />
+        </PageContext.Provider>
+    );
 }
 
 export default App;
