0f103ad176afc2b358823f2d4eede70441b16e01
Yerbrown <yerbrown98@gmail.com>
2024-12-16 16:25:22 +0100

    Added new changes in home css

--- mi-aplicacion/src/components/sections/LatestStories.jsx
+++ mi-aplicacion/src/components/sections/LatestStories.jsx
@@ -1,43 +1,49 @@
 function LatestStories() {
     return (
-        <>
+        <div className="latest-stories-section">
+            <div className="top-div">
+                <h5>· Stories</h5>
+            </div>
+            <div id="first-div">
+                <div id="third-div">
+                    <h3 id="travel">TRAVEL</h3>
+                    <h3 id="consciously">CONSCIOUSLY</h3>
+                    <div>
+                        <div>
+                            <img
+                                src="https://www.papagayobike.com/images/blog/estrategias-de-recuperacion-para-ciclistas-descanso-estiramiento-y-mas-1.jpg"
+                                alt=""
+                                width={400}
+                            />
+                            <h4>From France to Navarra</h4>
+                            <p>25/04/2023 - 30/04/2023</p>
+                        </div>
+                        <div>
+                            <img
+                                src="https://www.papagayobike.com/images/blog/estrategias-de-recuperacion-para-ciclistas-descanso-estiramiento-y-mas-1.jpg"
+                                alt=""
+                                width={400}
+                            />
+                            <h4>From France to Navarra</h4>
+                            <p>25/04/2023 - 30/04/2023</p>
+                        </div>
+                        <div>
+                            <img
+                                src="https://www.papagayobike.com/images/blog/estrategias-de-recuperacion-para-ciclistas-descanso-estiramiento-y-mas-1.jpg"
+                                alt=""
+                                width={400}
+                            />
+                            <h4>From France to Navarra</h4>
+                            <p>25/04/2023 - 30/04/2023</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div>
                 <h5>Lates Stories</h5>
                 <a href="">Read All Stories</a>
             </div>
-            <div>
-                <div>
-                    <div>
-                        <img
-                            src="https://www.papagayobike.com/images/blog/estrategias-de-recuperacion-para-ciclistas-descanso-estiramiento-y-mas-1.jpg"
-                            alt=""
-                            width={400}
-                        />
-                        <h4>From France to Navarra</h4>
-                        <p>25/04/2023 - 30/04/2023</p>
-                    </div>
-                    <div>
-                        <img
-                            src="https://www.papagayobike.com/images/blog/estrategias-de-recuperacion-para-ciclistas-descanso-estiramiento-y-mas-1.jpg"
-                            alt=""
-                            width={400}
-                        />
-                        <h4>From France to Navarra</h4>
-                        <p>25/04/2023 - 30/04/2023</p>
-                    </div>
-                    <div>
-                        <img
-                            src="https://www.papagayobike.com/images/blog/estrategias-de-recuperacion-para-ciclistas-descanso-estiramiento-y-mas-1.jpg"
-                            alt=""
-                            width={400}
-                        />
-                        <h4>From France to Navarra</h4>
-                        <p>25/04/2023 - 30/04/2023</p>
-                    </div>
-                </div>
-                <div>.....</div>
-            </div>
-        </>
+        </div>
     );
 }
 export default LatestStories;
