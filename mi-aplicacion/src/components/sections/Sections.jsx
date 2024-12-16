f20b69bfa1912df2b38a3f938e3953d7d4007982
Yerbrown <yerbrown98@gmail.com>
2024-12-16 23:37:42 +0100

    Change carousel image order

--- mi-aplicacion/src/components/sections/Sections.jsx
+++ mi-aplicacion/src/components/sections/Sections.jsx
@@ -9,8 +9,8 @@ import HomeFooter from "./HomeFooter";
 
 function Sections() {
     const carruselImages = [
-        "https://images.pexels.com/photos/29637789/pexels-photo-29637789/free-photo-of-paisaje-costero-escenico-con-oceano-y-rocas.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         "https://images.pexels.com/photos/3450182/pexels-photo-3450182.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        "https://images.pexels.com/photos/29637789/pexels-photo-29637789/free-photo-of-paisaje-costero-escenico-con-oceano-y-rocas.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         "https://images.pexels.com/photos/6272170/pexels-photo-6272170.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         "https://images.pexels.com/photos/3021724/pexels-photo-3021724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         "https://images.pexels.com/photos/12652444/pexels-photo-12652444.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
